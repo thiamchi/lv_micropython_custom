1c14e51c2860f935b1491d2bcf1740fefd154ab8
FPGA <thiamchi.gan@gmail.com>
2022-03-04 21:15:08 +0800

    Initial commit lvgl

---
+++ extmod/ftp/manifest.py
@@ -0,0 +1,8 @@
+# This list of frozen files doesn't include task.py because that's provided by the C module.
+freeze(
+    "..",
+    (
+        "ftp/uftpd.py",
+    ),
+    opt=3,
+)
---
+++ ports/esp32/boards/GENERIC_SPIRAM/mpconfigboard.mk
@@ -0,0 +1,3 @@
+SDKCONFIG += boards/sdkconfig.base
+SDKCONFIG += boards/sdkconfig.spiram
+SDKCONFIG += boards/sdkconfig.240mhz
